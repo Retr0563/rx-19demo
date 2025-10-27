c092a48318a30c57fe024e671474f3d948d13124
Retr0 <nanayawbee2014@gmail.com>
2025-10-27 12:27:26 +0000

    tabs in R-19

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import Apps from "./components/Apps";
+// import Apps from "./components/Apps";
 
 const App = () => {
-  return (
-    <div>
-      <Apps />
-    </div>
-  );
+  return <div>{/* <Apps /> */}</div>;
 };
 export default App;
